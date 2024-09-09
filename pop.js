d243898a0490c641910f5ad5269503c928ca39af
kavya <srikavya14085@gmail.com>
2024-09-09 17:28:23 +0530

    pop operation are performed

---
+++ pop.js
@@ -0,0 +1,7 @@
+let vegetables=['brinjal', 'carrot','onion'];
+console.log(vegetables.pop())
+console.log(vegetables)
+console.log(vegetables.shift())
+vegetables.push('beetroot');
+console.log(vegetables)
+console.log(vegetables.reverse());
